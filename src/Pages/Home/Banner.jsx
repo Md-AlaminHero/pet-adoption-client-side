b79de56f4a9068b745aa9b772df1240c52acc240
Md-Alaminhero <alaminrajf@gmail.com>
2025-07-12 14:09:20 +0600

    added simple banner slider in Home

---
+++ src/Pages/Home/Banner.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+
+const Banner = () => {
+    return (
+        <Carousel autoPlay='true' infiniteLoop={true} showThumbs={false}>
+            <div>
+                <img src="https://i.postimg.cc/RF9WGK4h/slide-03.jpg" />
+                <p className="legend">Legend 1</p>
+            </div>
+            <div>
+                <img src="https://i.postimg.cc/nzHLqxGQ/slide-02.jpg" />
+                <p className="legend">Legend 2</p>
+            </div>
+            <div>
+                <img src="https://i.postimg.cc/vm4hbRVf/slide-01.jpg" />
+                <p className="legend">Legend 3</p>
+            </div>
+        </Carousel>
+    );
+};
+
+export default Banner;
