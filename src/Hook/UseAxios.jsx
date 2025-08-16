3857f46686659cbfbe6be0bcf5a7e9569480b4a8
Md-Alaminhero <alaminrajf@gmail.com>
2025-08-17 01:17:33 +0600

    correction the baseurl

--- src/Hook/UseAxios.jsx
+++ src/Hook/UseAxios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     // baseURL:`https://pet-adoption-server-gilt.vercel.app`
-    baseURL:`https://pet-adoption-server-side-three.vercel.app/`
+    baseURL:`https://pet-adoption-server-side-three.vercel.app`
    
 })
 
